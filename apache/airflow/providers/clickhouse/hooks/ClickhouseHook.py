dedbe500980111c90c939cc6f22b988fcd6fb0b8
zhangsh204 <zhangsh204@midea.com>
2024-08-13 21:27:06 +0800

    add http port

--- apache/airflow/providers/clickhouse/hooks/ClickhouseHook.py
+++ apache/airflow/providers/clickhouse/hooks/ClickhouseHook.py
@@ -1,5 +1,7 @@
 from typing import Dict, Any, Iterable, Union
+from urllib.parse import parse_qsl, quote, urlencode
 
+from airflow.exceptions import AirflowException
 from airflow.hooks.dbapi import DbApiHook
 from airflow.models.connection import Connection
 from clickhouse_driver import Client
@@ -23,12 +25,63 @@ class ClickhouseHook(DbApiHook):
     hook_name = 'ClickHouse'
     database = ''
 
-    @staticmethod
-    def get_ui_field_behaviour() -> Dict:
+    def _get_field(self, extra_dict, field_name):
+        backcompat_prefix = "extra__clickhouse__"
+        backcompat_key = f"{backcompat_prefix}{field_name}"
+        if field_name.startswith("extra__"):
+            raise ValueError(
+                f"Got prefixed name {field_name}; please remove the '{backcompat_prefix}' prefix "
+                f"when using this method."
+            )
+        if field_name in extra_dict:
+            import warnings
+
+            if backcompat_key in extra_dict:
+                warnings.warn(
+                    f"Conflicting params `{field_name}` and `{backcompat_key}` found in extras. "
+                    f"Using value for `{field_name}`.  Please ensure this is the correct "
+                    f"value and remove the backcompat key `{backcompat_key}`.",
+                    UserWarning,
+                    stacklevel=2,
+                )
+            return extra_dict[field_name] or None
+        return extra_dict.get(backcompat_key) or None
+
+    @property
+    def http_port(self) -> int:
+        conn = self.get_connection(self.click_conn_id)  # type: ignore[attr-defined]
+        extra_dict = conn.extra_dejson
+        field_value = self._get_field(extra_dict, "http_port")
+        try:
+            return int(field_value)
+        except ValueError:
+            raise AirflowException(
+                f"The http_port field should be a integer. "
+                f'Current value: "{field_value}" (type: {type(field_value)}). '
+                f"Please check the connection configuration."
+            )
+
+    @classmethod
+    def get_connection_form_widgets(cls) -> dict[str, Any]:
+        """Return connection widgets to add to connection form."""
+        from flask_appbuilder.fieldwidgets import BS3TextFieldWidget
+        from flask_babel import lazy_gettext
+        from wtforms import IntegerField
+
+        return {
+            "http_port": IntegerField(
+                lazy_gettext("HTTP default port"),
+                widget=BS3TextFieldWidget(),
+                default=8123,
+            ),
+        }
+
+    @classmethod
+    def get_ui_field_behaviour(cls) ->  dict[str, Any]:
         """Returns custom field behaviour"""
         return {
             "hidden_fields": ['extra'],
-            "relabeling":    {'schema': 'Database'},
+            "relabeling": {'schema': 'Database', 'port': "Native Protocal port"},
         }
 
     def get_conn(self, conn_name_attr: str = None) -> Client:
@@ -42,6 +95,9 @@ class ClickhouseHook(DbApiHook):
         password: str = conn.password
         database: str = conn.schema
         click_kwargs = conn.extra_dejson.copy()
+        if "http_port" in click_kwargs:
+            click_kwargs.pop("http_port")
+            click_kwargs.pop("extra__clickhouse__http_port")
         if password is None:
             password = ''
         click_kwargs.update(port=port)
@@ -49,10 +105,59 @@ class ClickhouseHook(DbApiHook):
         click_kwargs.update(password=password)
         if database:
             click_kwargs.update(database=database)
-
         result = Client(host or 'localhost', **click_kwargs)
         result.connection.connect()
         return result
+
+    def get_uri(self) -> str:
+        """Return connection in URI format."""
+
+        # conn = self.get_connection(getattr(self, self.conn_name_attr))
+        # conn.schema = self.database or conn.schema
+        # http_port = conn.extra_dejson.get("http_port", 8123)
+        # conn.extra
+        # uri = conn.get_uri().replace(f":{conn.port}", f":{http_port}")
+        # return uri
+
+        conn = self.get_connection(getattr(self, self.conn_name_attr))
+        uri = f"{self.conn_type.lower().replace('_', '-')}://"
+
+
+        if conn.host and "://" in conn.host:
+            protocol, host = conn.host.split("://", 1)
+        else:
+            protocol, host = None, conn.host
+
+        if protocol:
+            uri += f"{protocol}://"
+
+        authority_block = ""
+        if conn.login is not None:
+            authority_block += quote(conn.login, safe="")
+
+        if conn.password is not None:
+            authority_block += ":" + quote(conn.password, safe="")
+
+        if authority_block > "":
+            authority_block += "@"
+            uri += authority_block
+
+        host_block = ""
+        if host:
+            host_block += quote(host, safe="")
+
+        if self.http_port:
+            if host_block == "" and authority_block == "":
+                host_block += f"@:{self.http_port}"
+            else:
+                host_block += f":{self.http_port}"
+
+        if conn.schema:
+            host_block += f"/{quote(conn.schema, safe='')}"
+
+        uri += host_block
+
+        return uri
 
     def run(self, sql: Union[str, Iterable[str]], parameters: dict = None,
             with_column_types: bool = True, **kwargs) -> Any:
